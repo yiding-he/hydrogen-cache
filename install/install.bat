5c6cff29bdae4b8ddf66b957acf5e7e7ffb17017
Yiding He <yiding.he@gmail.com>
2019-02-05 20:37:48 +0800

    ...

--- install/install.bat
+++
@@ -1,1 +0,0 @@
-mvn install:install-file -Dfile=target/xz-ajiaedu-common-3.0-SNAPSHOT.jar
--- install/install.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-mvn install:install-file -Dfile=target/xz-ajiaedu-common-3.0-SNAPSHOT.jar
